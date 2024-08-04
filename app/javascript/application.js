b5d90fefcc111d86a898f145e6b5fab252499dc0
kotake <akimotorina10@gmail.com>
2024-08-04 11:54:00 +0900

    add/js

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", test); // turbolinksを使用している場合はDOMContentLoadedを使用
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("../test");
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  
+  root "tests#index"
+  resources :tests, only: [:index, :create]
 
   get '*path', controller: 'application', action: 'render_404'
 end
